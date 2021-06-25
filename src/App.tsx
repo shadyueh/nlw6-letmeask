bfca887d5817db3740f4d367c21bb796d8e3e4b7
Held Grijo <held.grijo.dpeam@gmail.com>
2021-06-25 16:20:42 -0400

    feat(button): Button click counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Button } from './components/Button';
 function App() {
   return (
     <div>
-        <Button>Clique aqui</Button>
         <Button />
         <Button />
         <Button />
-        <Button>Zeta</Button>
+        <Button />
+        <Button />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
-// props
-type ButtonProps = {
-    children?: string;
-}
+import { useState } from "react";
 
 // named export
-export function Button(props:ButtonProps) {
+export function Button() {
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter+1);
+    }
+
     return (
-        <button>{props.children || 'default'}</button>
+        <button onClick={increment}>{counter}</button>
     )
 }
