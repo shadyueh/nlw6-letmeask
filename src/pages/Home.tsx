29ef5c2c1dafe5b1c53d4c82aef4e1d5173aa2f7
Held Grijo <held.grijo.dpeam@gmail.com>
2021-07-06 16:39:20 -0400

    feat(auth): Firebase authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,19 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
+import { auth, firebase } from "../services/firebase";
 
 export function Home() {
 
   const history = useHistory();
 
-  function navigateToNewRoom() {
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+    });
+
     history.push('/rooms/new');
   }
   
@@ -25,7 +32,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="LetMeAsk" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
