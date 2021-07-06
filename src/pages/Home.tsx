35bb93d147b81aa073a4ef9a7fae35272b37e22f
Held Grijo <held.grijo.dpeam@gmail.com>
2021-07-06 13:01:40 -0400

    feat(auth): Update styles to match design specifications

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,24 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import "../styles/auth.scss";
+
 export function Home() {
   return(
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas de sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="LetMeAsk" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>Ou entre em uma sala</div>
+          <div className="separator">Ou entre em uma sala</div>
           <form action="">
             <input type="text" placeholder="Digite o código da sala" />
             <button type="submit">
