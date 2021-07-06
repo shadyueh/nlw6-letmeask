082172b6214da0393d542ab2eb3f6c74a864d7ec
Held Grijo <held.grijo.dpeam@gmail.com>
2021-07-06 17:28:12 -0400

    feat(auth): Auth Context

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,51 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from './pages/NewRoom';
+import { auth, firebase } from "./services/firebase";
 
-export const TestContext = createContext({}) as any;
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
 
-  const [value, setValue] = useState('Teste');
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider);
+  
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error("Missing information from Google Account");
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      });
+    }
+  }
 
   return (
     <BrowserRouter>
-      <TestContext.Provider value={{value, setValue}}>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
       <Route path="/" exact component={Home} />
       <Route path="/rooms/new" component={NewRoom} />
-      </TestContext.Provider>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
+import { useContext } from "react";
 import { useHistory } from "react-router-dom";
-
-import { auth, firebase } from "../services/firebase";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
+
+import { AuthContext } from "../App";
 import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
@@ -12,14 +13,12 @@ import "../styles/auth.scss";
 export function Home() {
 
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-    });
-
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
     history.push('/rooms/new');
   }
   
